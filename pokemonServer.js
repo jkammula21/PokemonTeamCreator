7d6f48dd1f66cf6019bf178c4772281ba78aa2ee
jkammula21 <jkammula21@gmail.com>
2025-05-10 17:17:40 -0400

    add works

--- pokemonServer.js
+++ pokemonServer.js
@@ -56,30 +56,30 @@ async function getData(endpoint) {
    }
 }
 
-async function addApplication(name) {
-   const databaseName = process.env.MONGO_DB_NAME;
-   const collectionName = process.env.MONGO_COLLECTION;
-   const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.p9pyx5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-   const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+// async function addApplication(name) {
+//    const databaseName = process.env.MONGO_DB_NAME;
+//    const collectionName = process.env.MONGO_COLLECTION;
+//    const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.p9pyx5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//    const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 
-   let url = "https://pokeapi.co/api/v2";
-   let query = "/pokemon";
-   let newName = "/" + name;
-   let endpoint = url + query + newName;
-   try {
-      await client.connect();
-      const database = client.db(databaseName);
-      const collection = database.collection(collectionName);
-      let res = await fetch(endpoint);
-      let data = await res.json();
-      const application = { name: data.name, height: parseFloat(data.height), weight: parseFloat(data.weight)};
-      let result = await collection.insertOne(application);
-   } catch (e) {
-      console.error(e);
-   } finally {
-      await client.close();
-   }
-}
+//    let url = "https://pokeapi.co/api/v2";
+//    let query = "/pokemon";
+//    let newName = "/" + name;
+//    let endpoint = url + query + newName;
+//    try {
+//       await client.connect();
+//       const database = client.db(databaseName);
+//       const collection = database.collection(collectionName);
+//       let res = await fetch(endpoint);
+//       let data = await res.json();
+//       const application = { name: data.name, height: parseFloat(data.height), weight: parseFloat(data.weight)};
+//       let result = await collection.insertOne(application);
+//    } catch (e) {
+//       console.error(e);
+//    } finally {
+//       await client.close();
+//    }
+// }
 
 
 
@@ -94,14 +94,16 @@ async function addApplication(name) {
    let query = "/pokemon";
    let newName = "/" + name;
    let endpoint = url + query + newName;
+   console.log(endpoint)
    try {
       await client.connect();
       const database = client.db(databaseName);
       const collection = database.collection(collectionName);
       let res = await fetch(endpoint);
       let data = await res.json();
+      console.log()
       const application = { name: data.name, height: parseFloat(data.height), weight: parseFloat(data.weight)};
-      consoloe.log(application);
+      console.log(application);
       let result = await collection.insertOne(application);
    } catch (e) {
       console.error(e);
@@ -123,7 +125,7 @@ app.get("/teamCreator", (request, response) => {
 app.post("/teamCreator", (request, response) => {
    const {name} = request.body;
    addApplication(name);
-   const variables = {portNumber: portNumber, teamTable: "hello!"};
-   response.render("teamCreator", variables);
+   // const variables = {portNumber: portNumber, teamTable: "hello!"};
+   // response.render("teamCreator", variables);
 })
    
